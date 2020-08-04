e6886d3fb29ec9bb9924d131bfde0be56f2c7ceb
be320 <ahmedbahaa.3@hotmail.com>
2020-08-04 18:59:18 +0200

    Models Implemented

---
+++ backend/models/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+class Category
+{
+    protected $id;
+    protected $name;
+
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
