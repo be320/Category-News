fd606096aea640eddba49bebd0c073a29e24bacc
be320 <ahmedbahaa.3@hotmail.com>
2020-08-04 18:51:14 +0200

    DBConnection file implemented

--- backend/lib/DBConnection.php
+++ backend/lib/DBConnection.php
@@ -19,10 +19,15 @@ class DBConnection
         }
         try{
             self::$conn = new PDO("mysql:host=$dbServerName;dbname=$dbName;charset=utf8", $dbUserName, $dbPassword);
-            
-        }
-        catch(PDOException $e){
+
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         }
+        catch(PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+            exit();
+        }
+
+        return self::$conn;
     }
 }
