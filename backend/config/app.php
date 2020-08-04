db1808eb550fad105db2024ade955d0067de2e90
be320 <ahmedbahaa.3@hotmail.com>
2020-08-04 18:32:45 +0200

    Project Initiallized

---
+++ backend/config/app.php
@@ -0,0 +1,7 @@
+<?php
+global $dbServerName, $dbPassword, $dbName, $dbUserName;
+
+$dbServerName = 'localhost';
+$dbUserName = 'root';
+$dbPassword = '';
+$dbName = 'task1_db';
---
+++ backend/lib/DBConnection.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once(__DIR__ . '/../config/app.php');
+
+class DBConnection
+{
+    protected static $conn;
+
+    protected function __construct()
+    {
+    }
+
+    public static function connect()
+    {
+        global $dbServerName, $dbPassword, $dbName, $dbUserName;
+
+        if(!empty(self::$conn)){
+            return self::$conn;
+        }
+        try{
+            self::$conn = new PDO("mysql:host=$dbServerName;dbname=$dbName;charset=utf8", $dbUserName, $dbPassword)
+        }
+        catch(PDOException $e){
+
+        }
+    }
+}
