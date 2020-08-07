1368a73c06d0a1d235f38ad9519a63649a62d2a9
be320 <ahmedbahaa.3@hotmail.com>
2020-08-07 22:21:04 +0200

    Database Implemented

--- backend/DB/DB.sql
+++ backend/DB/DB.sql
@@ -1,7 +1,7 @@
 -- Database: `croco_news`
 
 CREATE DATABASE IF NOT EXISTS `croco_news` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `croco-news`
+USE `croco_news`
 
 
 -- Table structure for table `category`
@@ -33,4 +33,20 @@ CREATE TABLE IF NOT EXISTS `news` (
     PRIMARY KEY(`news_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Table structure for table `news`
+-- Table structure for table `news_in_category`
+
+DROP TABLE IF EXISTS `news_in_category`;
+CREATE TABLE IF NOT EXISTS `news_in_category` (
+    `category_id` int(11) NOT NULL,
+    `news_id` int(11) NOT NULL,
+    PRIMARY KEY (`category_id`,`news_id`),
+    KEY `news_in_category_ibfk_2` (`news_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Constraints for table `news_in_category`
+
+ALTER TABLE `news_in_category`
+  ADD CONSTRAINT `news_in_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `news_in_category_ibfk_2` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
