e004edf6812dc1eedb4321180db5603adfe7d817
be320 <ahmedbahaa.3@hotmail.com>
2020-08-08 14:39:53 +0200

    CORS Problem fixed

--- backend/api/createCategory.php
+++ backend/api/createCategory.php
@@ -1,6 +1,10 @@
 <?php
 require_once (__DIR__."/../repository/CategoryRepository.php");
 
+
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-Requested-With,Origin,Content-Type,Accept");
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 
