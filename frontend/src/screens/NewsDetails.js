1b9e831000bca195232d560c2a6b8022c408c5c5
be320 <ahmedbahaa.3@hotmail.com>
2020-08-14 15:41:05 +0200

    Displayed News Details Successfully

--- frontend/src/screens/NewsDetails.js
+++ frontend/src/screens/NewsDetails.js
@@ -1,32 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import TinyEditor from '../sideComponents/TinyEditor';
-import Header from "../sideComponents/Header";
 
-const NewsDetails = ({newsID}) => {
+const NewsDetails = ({ newsID }) => {
+  const [news, setNews] = useState({});
 
-    const [news,setNews] = useState({});
+  const load = async () => {
+    const data = await axios.get(
+      "http://localhost/Category-News/backend/api/getSingleNews.php",
+      {
+        params: {
+          id: newsID
+        }
+      }
+    );
+    setNews(data.data);
+    console.log(data.data);
+  };
 
-    const load = async() => {
-        const data = await axios.get(
-            "http://localhost/Category-News/backend/api/getSingleNews.php", {
-              params: {
-                id: newsID
-              }
-            }
-          );
-          setNews(data.data);
-          console.log(data.data)
-    }
+  useEffect(() => {
+    load();
+  }, []);
 
-    useEffect(()=>{
-        load();
-    },[])
-
-    return(
-       <TinyEditor />
-    )
-
-}
+  return (
+    <div className="news-details-container">
+      {news ? (
+        <div>
+          <img
+            className="news-details-image"
+            src={"data:image/*;base64, " + news.image}
+            alt="samsung"
+            width="900px"
+            height="600px"
+          />
+          <div className="news-details-title">{news.title}</div>
+          <div className="news-details-content"
+            dangerouslySetInnerHTML={{
+              __html: news.content
+            }}
+          ></div>
+           <div className="news-details-content">
+             Copyrights to : {news.author}
+           </div>
+           <div className="news-details-content">
+             For more information : <a href={news.link} alt ="link" >News Source</a>
+           </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
 export default NewsDetails;
